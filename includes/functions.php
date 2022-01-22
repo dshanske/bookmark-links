0b0101f1ecaaf4ff2d85bd75e839695beb5cc7a4
David Shanske <david@shanske.com>
2022-01-22 02:49:46 +0000

    Add conditions to ensure functions will not conflict

--- includes/functions.php
+++ includes/functions.php
@@ -5,63 +5,78 @@
  * @package Bookmark_Links
  */
 
-/**
- * Adds meta data field to a link.
- *
- * @param int    $link_id    Link ID.
- * @param string $meta_key   Metadata name.
- * @param mixed  $meta_value Metadata value.
- * @param bool   $unique     Optional, default is false. Whether the same key should not be added.
- * @return int|false Meta ID on success, false on failure.
- */
-function add_link_meta( $link_id, $meta_key, $meta_value, $unique = false ) {
-	return add_metadata( 'link', $link_id, $meta_key, $meta_value, $unique );
+
+if ( ! function_exists( 'add_link_meta' ) ) {
+	/**
+	 * Adds meta data field to a link.
+	 *
+	 * @param int    $link_id    Link ID.
+	 * @param string $meta_key   Metadata name.
+	 * @param mixed  $meta_value Metadata value.
+	 * @param bool   $unique     Optional, default is false. Whether the same key should not be added.
+	 * @return int|false Meta ID on success, false on failure.
+	 */
+	function add_link_meta( $link_id, $meta_key, $meta_value, $unique = false ) {
+		return add_metadata( 'link', $link_id, $meta_key, $meta_value, $unique );
+	}
 }
 
-/**
- * Removes metadata matching criteria from a link.
- *
- * You can match based on the key, or key and value. Removing based on key and
- * value, will keep from removing duplicate metadata with the same key. It also
- * allows removing all metadata matching key, if needed.
- *
- * @param int    $link_id    Link ID.
- * @param string $meta_key   Metadata name.
- * @param mixed  $meta_value Optional. Metadata value.
- * @return bool True on success, false on failure.
- */
-function delete_link_meta( $link_id, $meta_key, $meta_value = '' ) {
-	return delete_metadata( 'link', $link_id, $meta_key, $meta_value );
+
+if ( ! function_exists( 'delete_link_meta' ) ) {
+
+	/**
+	 * Removes metadata matching criteria from a link.
+	 *
+	 * You can match based on the key, or key and value. Removing based on key and
+	 * value, will keep from removing duplicate metadata with the same key. It also
+	 * allows removing all metadata matching key, if needed.
+	 *
+	 * @param int    $link_id    Link ID.
+	 * @param string $meta_key   Metadata name.
+	 * @param mixed  $meta_value Optional. Metadata value.
+	 * @return bool True on success, false on failure.
+	 */
+	function delete_link_meta( $link_id, $meta_key, $meta_value = '' ) {
+		return delete_metadata( 'link', $link_id, $meta_key, $meta_value );
+	}
 }
 
-/**
- * Retrieve meta field for a link.
- *
- * @param int    $link_id link ID.
- * @param string $key     Optional. The meta key to retrieve. By default, returns data for all keys.
- * @param bool   $single  Whether to return a single value.
- * @return mixed Will be an array if $single is false. Will be value of meta data field if $single is true.
- */
-function get_link_meta( $link_id, $key = '', $single = false ) {
-	return get_metadata( 'link', $link_id, $key, $single );
+
+
+if ( ! function_exists( 'get_link_meta' ) ) {
+	/**
+	 * Retrieve meta field for a link.
+	 *
+	 * @param int    $link_id link ID.
+	 * @param string $key     Optional. The meta key to retrieve. By default, returns data for all keys.
+	 * @param bool   $single  Whether to return a single value.
+	 * @return mixed Will be an array if $single is false. Will be value of meta data field if $single is true.
+	 */
+	function get_link_meta( $link_id, $key = '', $single = false ) {
+		return get_metadata( 'link', $link_id, $key, $single );
+	}
 }
 
-/**
- * Update link meta field based on link ID.
- *
- * Use the $prev_value parameter to differentiate between meta fields with the
- * same key and link ID.
- *
- * If the meta field for the user does not exist, it will be added.
- *
- * @param int    $link_id   Link ID.
- * @param string $meta_key   Metadata key.
- * @param mixed  $meta_value Metadata value.
- * @param mixed  $prev_value Optional. Previous value to check before removing.
- * @return int|bool Meta ID if the key didn't exist, true on successful update, false on failure.
- */
-function update_link_meta( $link_id, $meta_key, $meta_value, $prev_value = '' ) {
-	return update_metadata( 'link', $link_id, $meta_key, $meta_value, $prev_value );
+
+
+if ( ! function_exists( 'update_link_meta' ) ) {
+	/**
+	 * Update link meta field based on link ID.
+	 *
+	 * Use the $prev_value parameter to differentiate between meta fields with the
+	 * same key and link ID.
+	 *
+	 * If the meta field for the user does not exist, it will be added.
+	 *
+	 * @param int    $link_id   Link ID.
+	 * @param string $meta_key   Metadata key.
+	 * @param mixed  $meta_value Metadata value.
+	 * @param mixed  $prev_value Optional. Previous value to check before removing.
+	 * @return int|bool Meta ID if the key didn't exist, true on successful update, false on failure.
+	 */
+	function update_link_meta( $link_id, $meta_key, $meta_value, $prev_value = '' ) {
+		return update_metadata( 'link', $link_id, $meta_key, $meta_value, $prev_value );
+	}
 }
 
 /**
